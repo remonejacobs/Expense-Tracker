d8d3697901e74287931f6c0ebd8c6e2af955722e
yonela.bam <yonela.bam@cullinansystems.co.za>
2026-01-19 11:32:45 +0200

    Created the form and made changes to the model

--- expense_tracker/forms.py
+++ expense_tracker/forms.py
@@ -1,5 +1,45 @@
 from django                                                                     import forms
 
 from crispy_forms.helper                                                        import FormHelper
-from crispy_forms.layout                                                        import Layout, Row, Column, Fieldset
+from crispy_forms.layout                                                        import Layout, Row, Column, Fieldset, Submit
+
+from .models                                                                    import Expense
+
+class NewExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = '__all__'
+        labels = {
+            'amount': 'Amount (R)',
+            'date': 'Date',
+            'category': 'Category',
+            'expense_name': 'Expense Name',
+            'description': 'Description (Optional)',
+        }
+        widgets = {
+            'amount': forms.NumberInput(attrs={'step': '0.01'}),
+            'date': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset('New Expense',),
+            Row(
+                Column('amount', css_class='col-md-4'),
+                Column('date', css_class='col-md-4'),
+            ),
+            Row(
+                Column('category', css_class='col-md-4'),
+                Column('expense_name', css_class='col-md-4'),
+            ),
+            Row(
+                Column('description', css_class='col-md-6'),
+            ),
+            Row(
+                Column(Submit('submit', 'Add Expense', css_class='btn btn-primary')),
+            )
+        )
 
