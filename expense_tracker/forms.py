ded14fa8f893ef75f9285bf637f73ab146128e85
yonela.bam <yonela.bam@cullinansystems.co.za>
2026-01-19 10:50:46 +0200

    Created the model

---
+++ expense_tracker/forms.py
@@ -0,0 +1,5 @@
+from django                                                                     import forms
+
+from crispy_forms.helper                                                        import FormHelper
+from crispy_forms.layout                                                        import Layout, Row, Column, Fieldset
+
