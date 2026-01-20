ab41aff9542d753ffba06ba3cc8905422866e04d
remone.jacobs <remone.jacobs@cullinansystems.co.za>
2026-01-20 13:32:12 +0200

    remone-fixed url issue and added the expense_list view

--- expense_tracker/views.py
+++ expense_tracker/views.py
@@ -7,10 +7,20 @@ from .forms                                                 import NewExpenseFor
 # Create your views here.
 class ExpenseTrackerView(View):
 
-    template_name = 'base.html'
+    template_name = 'overview.html'
 
     def get(self, request):
         form = NewExpenseForm()
         return render(request, self.template_name, {'form': form})
 
+class ExpenseListView(View):
+    template_name = 'expense_list.html'
 
+    def get(self, request):
+        form = NewExpenseForm()
+
+        context = {'form': form}
+
+        render_response = render(request, self.template_name, context)
+
+        return render_response
